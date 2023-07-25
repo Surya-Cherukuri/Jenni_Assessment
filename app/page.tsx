17af950a11e70b5e2a5bc515b14af08c692f5add
Surya <surya@Suryas-MacBook-Air.local>
2023-07-25 16:48:03 -0700

    Adding the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image'
-import Tiptap from '../components/Tiptap'
+'use client';
+import dynamic from 'next/dynamic'
 
+const RichTextEditor = dynamic(() => import('../components/RichTextEditor'), {
+  ssr: false
+})
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <Tiptap />
+      <RichTextEditor />
       </div>
     </main>
   )
