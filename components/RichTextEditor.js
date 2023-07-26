4d8adc69b0e5d618eefb8cfe671f9cee66b7a949
Surya <surya@Suryas-MacBook-Air.local>
2023-07-25 17:45:17 -0700

    Used TiptapCollabProvider instead of HocuspocusProvider

--- components/RichTextEditor.js
+++ components/RichTextEditor.js
@@ -1,9 +1,14 @@
-import { HocuspocusProvider } from '@hocuspocus/provider'
+import { TiptapCollabProvider } from '@hocuspocus/provider'
+import CharacterCount from '@tiptap/extension-character-count'
 import Collaboration from '@tiptap/extension-collaboration'
 import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
+import Highlight from '@tiptap/extension-highlight'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import React, {
+  useCallback, useEffect,
   useState,
 } from 'react'
 import * as Y from 'yjs'
@@ -26,35 +31,60 @@ const getInitialUser = () => {
     color: getRandomColor(),
   }
 }
+const ydoc = new Y.Doc()
+const websocketProvider = new TiptapCollabProvider({
+  appId: 'EOK0LJK4',
+  name: 'Jenni',
+  token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2OTAzMzExMzUsIm5iZiI6MTY5MDMzMTEzNSwiZXhwIjoxNjkwNDE3NTM1LCJpc3MiOiJodHRwczovL2NvbGxhYi50aXB0YXAuZGV2IiwiYXVkIjoiaGVsbG9Ac293bWl0aC5kZXYifQ.xaRDgL00_viiHTo9jM-7VULdXkkBAtzjTtemm0DxCTc',
+  document: ydoc,
+})
 export default function RichTextEditor() {
-  const ydoc = new Y.Doc()
-  const [currentUser] = useState(getInitialUser)
+  const [status, setStatus] = useState('connecting')
+  const [currentUser, setCurrentUser] = useState(getInitialUser)
 
-  const provider = new HocuspocusProvider({
-    url: 'ws://127.0.0.1:1234',
-    name: 'example-document',
-    document: ydoc
-  })
  
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({
+        history: false,
+      }),
+      Highlight,
+      TaskList,
+      TaskItem,
+      CharacterCount.configure({
+        limit: 10000,
+      }),
       Collaboration.configure({
-        document: ydoc
+        document: ydoc,
       }),
       CollaborationCursor.configure({
-        provider,
-        user: { name: currentUser.name, color: currentUser.color }
-      })
+        provider: websocketProvider,
+      }),
     ],
-    content: '<p>Hello World! 🌎️</p>',
-    editorProps: {
-      attributes: {
-        class:
-          'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none'
-      }
-    }
   })
+
+  useEffect(() => {
+    // Update status changes
+    websocketProvider.on('status', event => {
+      setStatus(event.status)
+    })
+  }, [])
+
+    // Save current user to localStorage and emit to editor
+    useEffect(() => {
+      if (editor && currentUser) {
+        localStorage.setItem('currentUser', JSON.stringify(currentUser))
+        editor.chain().focus().updateUser(currentUser).run()
+      }
+    }, [editor, currentUser])
+
+  const setName = useCallback(() => {
+    const name = (window.prompt('Name') || '').trim().substring(0, 32)
+
+    if (name) {
+      return setCurrentUser({ ...currentUser, name })
+    }
+  }, [currentUser])
  
   return <EditorContent editor={editor} />
 }
